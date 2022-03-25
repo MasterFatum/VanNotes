43b5f8da93fd8b6366e06bf55c22d72b5680b52b
Иван Панфилов <pi.resonance@hotmail.com>
2022-03-25 15:20:01 +0300

    Добавьте файлы проекта.

---
+++ VanNotes/VanNotes/Data/NotesDB.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using VanNotes.Models;
+
+namespace VanNotes.Data
+{
+    internal class NotesDB
+    {
+        readonly SQLiteAsyncConnection db;
+
+        public NotesDB(string connectionString)
+        { 
+            db = new SQLiteAsyncConnection(connectionString);
+
+            db.CreateTableAsync<Note>().Wait();
+        }
+
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return db.Table<Note>().ToListAsync();
+        }
+
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return db.Table<Note>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.Id != 0)
+            {
+                return db.UpdateAsync(note);
+            }
+            else
+            {
+                return db.InsertAsync(note);
+            }
+        }
+
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+           return db.DeleteAsync(note);
+        }
+    }
+}
---
+++ VanNotes/VanNotes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace VanNotes.Models
+{
+    internal class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
